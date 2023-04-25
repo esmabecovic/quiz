e4d834fc15078d5ce36a4d293822d74db745105c
zeindelic@gmail.com <zeindelic@gmail.com>
2023-04-25 10:10:03 -0700

    show question

--- quiz.js
+++ quiz.js
@@ -1,6 +1,47 @@
+let questionText = document.querySelector("#questionTextP")
+let questionTextDiv = document.querySelector("#questionTextDiv")
 
+let main = document.querySelector("#main")
+let cardQuestion = document.querySelector("#cardQuestion")
+
+let allAnswers = document.querySelectorAll(".ans")
+let answer1Label = document.querySelector("#answer1Label")
+let answer2Label = document.querySelector("#answer2Label")
+let answer3Label = document.querySelector("#answer3Label")
+let answer4Label = document.querySelector("#answer4Label")
+
+brojac = 0
+
+function buttonClick(brojac) {
+    allAnswers.forEach(el => {
+      el.checked = false
+    el.onclick = () => {
+        brojac++
+        console.log(brojac);  
+    }
+    });
+
+    }
+    
+
+
+function ui() {
+    // for (let i = 0; i < questions.length; i++) {
+    //     const element = array[i];
+        
+    // }
+    console.log(questionText);
+    questionText.textContent = questions[brojac].question
+    answer1Label.textContent = questions[brojac].answers[0]
+    answer2Label.textContent = questions[brojac].answers[1]
+    answer3Label.textContent = questions[brojac].answers[2]
+    answer4Label.textContent = questions[brojac].answers[3]
+    
+}
 
 const shuffledQuestions = questions.sort((a, b) => 0.5 - Math.random());
 
 console.log(questions)
 console.log(shuffledQuestions);
+buttonClick(brojac)
+ui()
