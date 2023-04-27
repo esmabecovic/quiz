77a6fc61c858e5fb0357126c196de09f48a19d43
zeindelic@gmail.com <zeindelic@gmail.com>
2023-04-27 05:59:39 -0700

    resfresh dugme zavrseno

--- quiz.js
+++ quiz.js
@@ -1,90 +1,89 @@
-let questionText = document.querySelector("#questionTextP")
-let body = document.querySelector('body')
-let questionTextDiv = document.querySelector("#questionTextDiv")
+let questionText = document.querySelector("#questionTextP");
+let body = document.querySelector("body");
+let questionTextDiv = document.querySelector("#questionTextDiv");
+let main = document.querySelector("#main");
+let cardQuestion = document.querySelector("#cardQuestion");
+let allAnswers = document.querySelectorAll(".ans");
+let answer1Label = document.querySelector("#answer1Label");
+let answer2Label = document.querySelector("#answer2Label");
+let answer3Label = document.querySelector("#answer3Label");
+let answer4Label = document.querySelector("#answer4Label");
+let submitBtn = document.querySelector("#submitBtn");
+let btn = document.querySelector(".btn");
+let modal = document.querySelector(".myModal");
+let yesBtn = document.querySelector("#yes");
+let noBtn = document.querySelector("#no");
+let resetBtn = document.querySelector("#resetBtn")
 
-let main = document.querySelector("#main")
-let cardQuestion = document.querySelector("#cardQuestion")
-
-let allAnswers = document.querySelectorAll(".ans")
-let answer1Label = document.querySelector("#answer1Label")
-let answer2Label = document.querySelector("#answer2Label")
-let answer3Label = document.querySelector("#answer3Label")
-let answer4Label = document.querySelector("#answer4Label")
-let submitBtn = document.querySelector("#submitBtn")
-let btn = document.querySelector('.btn')
-let modal = document.querySelector('.myModal')
-let yesBtn = document.querySelector('#yes')
-let noBtn = document.querySelector('#no')
-
-brojac = 0
+brojac = 0;
 
 function buttonClick() {
-    allAnswers.forEach (el => {
-      el.checked = false
-    el.onclick = () => {
-        brojac++
-        console.log(brojac);  
+  allAnswers.forEach(
+    (el) => {
+      el.checked = false;
+      el.onclick = () => {
+        console.log(brojac);
+      };
+    },
+    (yesBtn.onclick = () => {
+      allAnswers.forEach((el) => {
+        // console.log(+el.id);
+        if (el.checked) {
+          if (+el.id === questions[brojac].correct_answer) {
+            console.log("tacno");
+            brojac++;
+            ui();
+            modal.style.display = "none";
+            // console.log(el.id);
+            // console.log(questions[i].correct_answer);
+            allAnswers.forEach((el) => {
+              el.checked = false;
+            });
+          } else {
+            console.log("netacno");
+            modal.style.display = "none";
+            // alert('Pokusajte ponovo!')
+            brojac++;
+            ui();
+            allAnswers.forEach((el) => {
+              el.checked = false;
+            });
+            // return false
+          }
+        }
+      });
+    })
+  );
+}
+noBtn.onclick = () => {
+  modal.style.display = "none";
+};
+
+function resetBtnFunc() {
+    resetBtn.onclick = () => {
+        location.reload()
     }
-},
-    yesBtn.onclick = () =>{
-        allAnswers.forEach(el =>{
-            // console.log(+el.id);
-            if(el.checked){
-                if(+el.id === questions[brojac].correct_answer){
-                    console.log("tacno")
-                    brojac++
-                    ui()
-                    modal.style.display = 'none'
-                    // console.log(el.id);
-                    // console.log(questions[i].correct_answer);
-                    allAnswers.forEach((el)=>{
-                        el.checked = false;
-                    })
-                }
-                else{
-                    console.log("netacno")
-                    modal.style.display = 'none'
-                    // alert('Pokusajte ponovo!')
-                    brojac++
-                    ui()
-                    allAnswers.forEach((el)=>{
-                        el.checked = false;
-                    })
-                    // return false
-                }
-            }
-        })
-    }
-    )} 
-    noBtn.onclick = () => {
-        modal.style.display = 'none'
-    }   
-
-
-    
-
-
-
-function ui() {
-    console.log(questionText);
-    questionText.textContent = questions[brojac].question
-    answer1Label.textContent = questions[brojac].answers[0]
-    answer2Label.textContent = questions[brojac].answers[1]
-    answer3Label.textContent = questions[brojac].answers[2]
-    answer4Label.textContent = questions[brojac].answers[3]
     
 }
+
+function ui() {
+  console.log(questionText);
+  questionText.textContent = questions[brojac].question;
+  answer1Label.textContent = questions[brojac].answers[0];
+  answer2Label.textContent = questions[brojac].answers[1];
+  answer3Label.textContent = questions[brojac].answers[2];
+  answer4Label.textContent = questions[brojac].answers[3];
+}
 function modalBtn() {
-    submitBtn.onclick = () => {
-           modal.style.display = 'flex'
-    }
- 
-    
+  submitBtn.onclick = () => {
+    modal.style.display = "flex";
+  };
 }
 const shuffledQuestions = questions.sort((a, b) => 0.5 - Math.random());
 
-console.log(questions)
+console.log(questions);
 console.log(shuffledQuestions);
-buttonClick()
-ui()
-modalBtn()
+buttonClick();
+ui();
+modalBtn();
+resetBtnFunc()
